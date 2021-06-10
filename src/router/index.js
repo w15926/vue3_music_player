f0ef05b8d2afa317939a888e1ed8d1a8526b8301
清风 <64196371+w15926@users.noreply.github.com>
2021-06-10 23:39:04 +0800

    Revert "Revert "提交源码""
    This reverts commit cadff8c2953e61b38550b708acb64d8813d944f0.

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '',
+    redirect: '/discover'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/home',
+    name: 'home',
+    component: () => import('@/views/Home'),
+    // redirect:'/home/discover',
+    meta: { title: '首页' },
+    children: [
+      {
+        path: '/discover',
+        component: () => import('../views/Home/Children/Discover.vue')
+      }
+    ]
+  },
+
 ]
 
 const router = createRouter({
---
+++ src/views/Home/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+    <header-bar />
+
+    <div class="box-sidebar">
+      <ul>
+        <li>发现音乐</li>
+        <li>推荐歌单</li>
+        <li>最新音乐</li>
+        <li>最新MV</li>
+      </ul>
+    </div>
+    <div class="box-main">
+      <router-view />
+    </div>
+    <div class="box-footer"></div>
+
+  </div>
+</template>
+
+<script>
+import HeaderBar from '@/components/HeaderBar'
+
+export default {
+  components: {
+    HeaderBar,
+    // Sidebar
+  },
+  setup() {
+
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container{
+  position: relative;
+
+  .box-sidebar{
+    width: 300px;
+    height: 700px;
+    background-color: wheat;
+  }
+
+  .box-main{
+    position: absolute;
+    top: 50px;
+    right: 0;
+    
+    width: 1140px;
+    height: 700px;
+    background-color: tan;
+  }
+
+  .box-footer{
+    width: 100%;
+    height: 50px;
+    background-color: tomato;
+  }
+}
+</style>
