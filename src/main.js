98c835e34f56befaa6f327adf6013aebf7fd0b7e
清风 <64196371+w15926@users.noreply.github.com>
2021-06-11 10:24:42 +0800

    Revert "Revert "Revert "test"""
    This reverts commit 4886ea036364721ddc8bf55c7d4166d2dfc45de4.

--- src/main.js
+++ src/main.js
@@ -6,5 +6,6 @@ import store from './store'
 import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
 import 'normalize.css/normalize.css'
+// test
 
 createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
