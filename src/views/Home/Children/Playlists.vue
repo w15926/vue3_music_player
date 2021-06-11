2fe651f83b3efdc0467c424c8ab517bbf9da1632
清风 <64196371+w15926@users.noreply.github.com>
2021-06-11 08:51:10 +0800

    提交源码

---
+++ src/views/Home/Children/Playlists.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+playlists
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
