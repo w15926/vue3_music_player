aed90275340a0ed4d2f96ac10e95439d43563d1f
清风 <64196371+w15926@users.noreply.github.com>
2021-06-11 09:31:11 +0800

    init

--- src/components/HeaderBar/index.vue
+++ src/components/HeaderBar/index.vue
@@ -1,22 +1,44 @@
 <template>
   <div class="header">
-    header
+    <el-row>
+      <el-col :span="6">
+        header
+      </el-col>
+      <el-col :span="12">
+      </el-col>
+      <el-col :span="3">
+        <el-input placeholder="请输入内容" v-model="inputValue" @keyup.enter="searchMusic" clearable />
+      </el-col>
+    </el-row>
+
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
   setup() {
+    let inputValue = ref('')
+    const searchMusic = () => {
+      if (inputValue.value.trim() !== '') {
+        console.log(1);
+      } else {
+        inputValue.value = ''
+      }
+    }
 
-
-    return {}
+    return {
+      inputValue,
+      searchMusic
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
 .header {
-  background-color: #ccc;
+  background-color: #F9F9F8;
   height: 50px;
 }
 </style>
